a06f18750acccb43175d1346e3ba4a716437709e
shan <759100319@qq.com>
2019-06-10 16:09:25 +0800

    shan

---
+++ 1901/vue/ssh/src/components/head.vue
@@ -0,0 +1,39 @@
+<template>
+   <van-nav-bar :title="title" :left-text="back" :left-arrow ="show" @click-left="onClickLeft"
+  @click-right="onClickRight">
+  <van-icon v-if="searchShow" name="search" slot="right"  />
+</van-nav-bar>
+
+</template>
+<script>
+    export default {
+      data(){
+          return {
+              searchShow:true
+          }
+      },
+      props:{
+        title:String,
+        back:String,
+        show:String
+        
+      },
+      mounted(){
+        this.searchShow = (this.$route.name !== "search")
+    },
+        methods: {
+          
+    onClickLeft() {
+      this.$router.go(-1);
+    },
+    onClickRight() {
+      this.$router.push({name:"ss"})
+    }
+  }
+    }
+</script>
+<style scoped>
+.van-icon {
+  font-size: 20px;
+}
+</style>
